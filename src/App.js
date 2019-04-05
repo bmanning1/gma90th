026c3112d9da34ca6472c478d1c6c17d72b49cbd
Becca Manning <rebm01@hotmail.co.uk>
2019-04-05 22:58:26 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import gma from './static/gma.jpg';
+import gmaVid from './static/gma.MOV';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { PG: true };
+        this.togglePGmode = this.togglePGmode.bind(this);
+     }
+
+     componentDidMount() {
+         document.body.classList.add('frog');
+     }
+
+     togglePGmode() {
+        if (this.state.PG) {
+            document.body.classList.remove('frog');
+            document.body.classList.add('danny');
+        } else {
+            document.body.classList.remove('danny');
+            document.body.classList.add('frog');
+        }
+        this.setState({ PG: !this.state.PG });
+     }
+
   render() {
     return (
       <div className="App">
-         <div>HAPPY 90th Grandma</div>
+         <div className="container">
+        	<div className="title">
+                HAPPY 90th
+                <br/>
+                Grandma
+        	</div>
+         </div>
+
+         <div className="buttonContainer">
+             <button
+                 className={`button ${this.state.PG ? 'frogButton' : 'dannyButton'}`}
+                 onClick={this.togglePGmode}>
+                 {this.state.PG ? 'Get rid of PG mode' : 'PG mode'}
+             </button>
+         </div>
+
+         {this.state.PG ?
+             <video muted src={gmaVid} type="video/mp4" className="gma" width="400" autoPlay loop></video>
+             : <iframe className="dannyVid" src="https://www.youtube.com/embed/Nh3ikHS6grE?autoplay=1"></iframe>}
+
       </div>
     );
   }
